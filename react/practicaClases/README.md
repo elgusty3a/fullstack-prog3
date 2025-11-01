4dee89fc8242a713ee391491fca7b6732320c412
Arias Gustavo <ariasgustavo3a@gmail.com>
2025-11-01 07:55:34 -0300

    creando al lista y fetch

--- react/practicaClases/README.md
+++ react/practicaClases/README.md
@@ -5,5 +5,5 @@ UseState
 2- Generar una tabla, la cual va a tener datos precargados y cada registro debe tener un boton que muestre por alert el registro.
 
 UseEffect
-1- Hacer uso de useEffect para consultar datos a un servicio externo y renderice en una lista.
+1- Hacer uso de useEffect para consultar datos a un servicio externo y renderice en una lista. (https://thronesapi.com/swagger/index.html?urls.primaryName=Game%20of%20Thrones%20API%20v2)
 2- Crear un contador con useState, este mismo debera tener un useEffect para que cada vez que el contador cambie el useEffect muestre un cartel con dicho cambio. Si el contador llega a 10, el mensaje debe ser  Llegaste a 10!.
--- react/practicaClases/src/App.jsx
+++ react/practicaClases/src/App.jsx
@@ -17,12 +17,22 @@ function App() {
       <Card data = {datosCard}>
         <p>Hola esto viene de App por children</p>
       </Card>
-      <h1>Crear un componente el cual muestre la variacion de un texto a medida que el usuario va escribiendo en el input</h1>
+      <br />
+      <h1>PRACTICAS DE USESTATE</h1>
+      <br />
+      <h1>1 Crear un componente el cual muestre la variacion de un texto a medida que el usuario va escribiendo en el input</h1>
       <div>
         <Inputcomponent />
       </div>
       <h1>Generar una tabla, la cual va a tener datos precargados y cada registro debe tener un boton que muestre por alert el registro</h1>
       <Tabla />
+      <br />
+      <h1>PRACTICAS DE USESTATE</h1>
+      <br />
+      <h1>1 Hacer uso de useEffect para consultar datos a un servicio externo y renderice en una lista.</h1>
+      <br />
+      <h1>2 Crear un contador con useState, este mismo debera tener un useEffect para que cada vez que el contador cambie el useEffect muestre un cartel con dicho cambio. Si el contador llega a 10, el mensaje debe ser  Llegaste a 10!.</h1>
+      <br />
     </>
   );
 }
